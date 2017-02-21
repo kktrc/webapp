2cbeb3a1f2c7f539d796470865a3198569db52c8
pzzls <moshenglei@icloud.com>
2017-02-21 19:26:34 +0800

    init

---
+++ src/main/java/me/pzzls/controller/TestController.java
@@ -0,0 +1,17 @@
+package me.pzzls.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Copyright (C) 2017 mosl, Inc.
+ * <p>内容摘要： </p>
+ * <p>其他说明： </p>
+ *
+ * @author <a href="mailto:moshenglei@icloud.com">mosl</a>
+ * @since 2017/2/21 下午7:05
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+}
